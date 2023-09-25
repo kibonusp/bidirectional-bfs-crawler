1705babb6dcde693e3f59e9dc8fb0a9896d28b9f
Kibon <gabriel120026@hotmail.com>
2023-09-25 18:14:49 -0300

    Enable route discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from multiprocessing import Process, Manager
 import networkx as nx
-import random
 import time
 
 # Retorna a maior componente do grafo
@@ -8,95 +7,126 @@ def get_largest_component(graph):
   Gcc = sorted(nx.connected_components(graph), key=len, reverse=True)
   return graph.subgraph(Gcc[0])
 
-N = 1000
+N = 10000
 GWS = get_largest_component(nx.watts_strogatz_graph(N, 10, 0.05))
-adj_matrix = nx.adjacency_matrix(GWS).todense()
+'''
+adj_matrix = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0]
+]
+'''
 
-def dfs_recursive(node, visitados, adj_matrix, conn_nodes):
+# visitados específico da dfs
+def dfs_recursive(node, visitados, adj_list, conn_node):
     visitados.add(node)
-    if node in conn_nodes:
+    if node == conn_node:
         return [node]
-
-    for i, neighbour in enumerate(adj_matrix[node]):
-        if neighbour == 1 and i not in visitados:
-            result = dfs_recursive(neighbour, visitados)
+    
+    if node not in adj_list:
+        return None
+    
+    for neighbor in adj_list[node]:
+        if neighbor not in visitados:
+            result = dfs_recursive(neighbor, visitados, adj_list, conn_node)
             if result is not None:
-                return [i] + result
+                return [node] + result
+            
+    return None
 
 
-def search(visitados_a, visitados_b, who, start_node, adj_matrix, stop, adj_list_a, adj_list_b):
+def search(visitados_a, visitados_b, who, start_node, adj_matrix, stop, adj_list_a, adj_list_b, route_a, route_b):
     if who == 'a':
         visitados = visitados_a
         opposite_visitados = visitados_b
         adj_list = adj_list_a
-        opposite_adj_list = adj_list_b
+        route = route_a
     else:
         visitados = visitados_b
         opposite_visitados = visitados_a
         adj_list = adj_list_b
-        opposite_adj_list = adj_list_a
+        route = route_b
+        
+    visitados[start_node] = 0
     
     queue = [start_node]
     while len(visitados) < 5000 and len(queue) > 0 and not stop.value:
         cur_node = queue.pop(0)
         adj_list[cur_node] = []
-        print(f'''
-              ===
-              FROM: {who}
-              ===
-              Cur Node: {cur_node}
-              visitados: {visitados}
-              opposite_visitados: {opposite_visitados}''')
         
         for i, neighbor in enumerate(adj_matrix[cur_node]):
-            adj_list[cur_node] += [i]
-            queue.append(i)
+            if neighbor == 1:
+                adj_list[cur_node] += [i]
+                queue.append(i)
             if neighbor != 0 and i not in visitados:
                 visitados[i] = 0
                 if i in opposite_visitados:
                     stop.value = True
-        time.sleep(1)
+        # time.sleep(1)
+    print(who, adj_list)
     
-    print(who, len(visitados), len(queue), stop.value, adj_list)
+    commons = sorted(list({x:visitados[x] for x in visitados.keys() if x in opposite_visitados}.keys()))
+    path_to_conn = dfs_recursive(start_node, set(), adj_list, commons[0])
     
-    #visitados_a = set(visitados_a)
-    #visitados_b = set(visitados_b)
-    #conn_nodes = visitados_a.intersection(visitados_b)
-    conn_nodes = visitados_a.keys() & visitados_b.keys()
-    print(conn_nodes)
+    route += path_to_conn
 
 
 if __name__ == '__main__':
+    start = time.time()
+    
     manager = Manager()
     visitados_a = manager.dict()
     visitados_b = manager.dict()
     adj_list_a = manager.dict()
     adj_list_b = manager.dict()
-    stop = manager.Value('stop', False)
+    route_a = manager.list()
+    route_b = manager.list()
     
-    process_b = Process(target=search, 
-                        args=(visitados_a, 
-                              visitados_b, 
-                              'b', 
-                              random.choice(list(GWS.nodes)), 
-                              adj_matrix, 
-                              stop,
-                              adj_list_a,
-                              adj_list_b))
+    # usado para parar a bfs em ambos os processos
+    # quando o nó estiver no visitados do processo oposto
+    stop = manager.Value('stop', False)
     
     process_a = Process(target=search, 
                         args=(visitados_a, 
                               visitados_b, 
                               'a', 
-                              random.choice(list(GWS.nodes)), 
-                              adj_matrix, 
+                              list(GWS.nodes)[0], 
+                              nx.adjacency_matrix(GWS).todense(), 
                               stop,
                               adj_list_a,
-                              adj_list_b))
+                              adj_list_b,
+                              route_a,
+                              route_b))
     
+    process_b = Process(target=search, 
+                        args=(visitados_a, 
+                              visitados_b, 
+                              'b', 
+                              list(GWS.nodes)[3720], 
+                              nx.adjacency_matrix(GWS).todense(), 
+                              stop,
+                              adj_list_a,
+                              adj_list_b,
+                              route_a,
+                              route_b))
     
-    process_b.start()
     process_a.start()
+    process_b.start()
     
     process_a.join()
     process_b.join()
+    
+    route_a_list = list(route_a)
+    route_b_list = list(route_b)
+    route_b_list.reverse()
+    
+    route = route_a_list + route_b_list[1:]
+    
+    end = time.time()
+    print("Route:", route)
+    print("Time elapsed: {:.2f}s".format(end-start))
